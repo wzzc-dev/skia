4b15411dc097de3fd6a4619b626cb97520e22d0c
wzzc-dev <wzzc@wzzc.dev>
2026-06-01 21:06:16 +0800

    Use QEMU for riscv64 CI

--- tools/skia_release/prepare_linux_riscv64.sh
+++ tools/skia_release/prepare_linux_riscv64.sh
@@ -4,4 +4,4 @@ set -o errexit -o nounset -o pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
-apt-get install build-essential git wget ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip python3 -y
+apt-get install build-essential ca-certificates git wget ninja-build fontconfig libfontconfig1-dev libglu1-mesa-dev curl zip python3 -y
